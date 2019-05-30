b90d473ee7f71c5c0df486050047ab13de623da4
cs4331 <cs4331@rit.edu>
2019-05-29 22:04:47 -0400

    fixed video files naming

--- removeme.py
+++ removeme.py
@@ -3,5 +3,5 @@ import os
 current_dir = os.path.dirname(os.path.realpath(__file__)) + "/videos"
 
 for filename in os.listdir(current_dir):
-    print(filename + "--> _" + filename.lower())
-    os.rename(current_dir + "/" + filename, current_dir + "/_" + filename.lower())
+    print(filename + "--> " + filename.lower()[2:])
+    os.rename(current_dir + "/" + filename, current_dir + "/" + filename.lower()[2:])
